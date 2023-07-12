3f9566e25fe728ba002e48367eb98c47136dbf8a
ReitaWada <hgu86364@kwansei.ac.jp>
2023-07-12 16:33:47 +0900

    R6_2

--- R6_2.py
+++ R6_2.py
@@ -3,9 +3,11 @@ import json
 import tkinter as tk
 
 root = tk.Tk()
-root.geometry("500x400")
+root.geometry("1350x950")
 
 canvas = tk.Canvas(root, bg = "white")
+
+canvas.pack(fill = tk.BOTH, expand = True)
 
 with zipfile.ZipFile('kabeposter.zip', 'r') as zfile:
     for name in zfile.namelist():
@@ -16,12 +18,8 @@ with zipfile.ZipFile('kabeposter.zip', 'r') as zfile:
                 data1 = people_data[0]
                 data2 = people_data[1]
                 
-                data1["pose_keypoints_2d"][0]
-                data1["pose_keypoints_2d"][0]
-                data1["pose_keypoints_2d"][0]
-                data1["pose_keypoints_2d"][0]
+                canvas.create_line(data1["pose_keypoints_2d"][6], data1["pose_keypoints_2d"][7], data1["pose_keypoints_2d"][15], data1["pose_keypoints_2d"][16])
                 
-                data1["pose_keypoints_2d"][0]
-                data1["pose_keypoints_2d"][0]
-                data1["pose_keypoints_2d"][0]
-                data1["pose_keypoints_2d"][0]
+                canvas.create_line(data2["pose_keypoints_2d"][6], data2["pose_keypoints_2d"][7], data2["pose_keypoints_2d"][15], data2["pose_keypoints_2d"][16])
+                
+                root.mainloop()
